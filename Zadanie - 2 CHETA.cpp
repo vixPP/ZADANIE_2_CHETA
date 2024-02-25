1d6d44c91f6d6c1b3b200dfbff6b7106b8a86810
vix <churink9@gmail.com>
2024-02-25 18:17:48 +0300

    first commit

---
+++ Zadanie - 2 CHETA.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct accounts
+{
+	int Number_Acc;
+	string Name;
+	double Balance;
+};
+
+void Update_Balance(accounts& a, double New_Balance)
+{
+	a.Balance = New_Balance;
+}
+
+int main()
+{
+	accounts a;
+	double New_Balance;
+
+	cout << "Введите номер счёта: ";
+	cin >> a.Number_Acc;
+
+	cout << "Введите имя владельца: ";
+	cin >> a.Name;
+
+	cout << "Введите баланс: ";
+	cin >> a.Balance;
+
+	cout << "Введите новый баланс: ";
+	cin >> New_Balance;
+
+	Update_Balance(a, New_Balance);
+
+	cout << "Ваш счёт: " << a.Name << ", " << a.Number_Acc << ", " << a.Balance;
+
+	return 0;
+}
